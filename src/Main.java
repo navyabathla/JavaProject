6282fc5f716ab77dc5d7c5244fa837d03a4c0a2d
Navya Bathla <navyabathla31@gmail.com>
2024-08-12 17:38:05 +0530

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-
-
+import java.util.Scanner; //importing package util and class scanner
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World!"); //prints hello world
         System.out.println("My first java program!");
+        Scanner sc= new Scanner(System.in); //scanner class is used to take input from the user
+        System.out.println(sc.nextInt()); //takes integer input
+        System.out.println(sc.nextDouble()); // takes double as input
+        System.out.println(sc.next()); //takes string input
+        System.out.println(sc.nextLine()); //give an empty line
     }
 }
