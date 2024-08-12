2e88504bcc32d8bdc986daf8eb4511d01ec7668e
Navya Bathla <navyabathla31@gmail.com>
2024-08-12 16:00:42 +0530

    Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!"); //prints hello world
+        System.out.println("My first java program!");
+    }
+}
